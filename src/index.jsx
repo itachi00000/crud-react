8d52c48771e636c3e36357d0bd6922d517fcb571
itachi00000 <>
2021-05-09 10:02:05 +0800

    wrap react-strict mode, replace browserRouter to hashRouter dueto error

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 // import './index.css';
 import './bootstrap.min.css';
 import App from './App';
@@ -8,9 +8,11 @@ import App from './App';
 // import Test from './Test';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
